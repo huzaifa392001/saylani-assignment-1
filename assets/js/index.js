59346b2002c2793d7a0240260f89680059e4d226
samgarcia14433 <108543947+samgarcia14433@users.noreply.github.com>
2023-03-28 02:53:59 +0500

    project completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,28 @@
 // function initialization
 $(window).on("load", function () {
-    $(window).scrollTop(0);
+    // $(window).scrollTop(0);
 });
 
 $(function () {
-    $('.intrestBox').on('click', function () {
-        $(this).toggleClass('selected');
-    })
-
-    $('.pillSwiper .pill').on('click', function () {
-        $('.pillSwiper').find('.pill').removeClass('active');
-        $(this).addClass('active')
-    })
-    pillSlider()
+    reviewSwiper()
 });
 
-function pillSlider() {
-    let swiper = new Swiper('.pillSwiper', {
-        slidesPerView: 13,
-        spaceBetween: 5,
-        // Navigation arrows
+function reviewSwiper() {
+    const reviewSlider = new Swiper('.swiper', {
+        speed: 1000,
+        loop: true,
+        autoplay:{
+            delay: 3500
+        },
+        autoplayDisableOnInteraction: false,
+        runCallbacksOnInit: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
     });
-    let swiperMin = new Swiper('.pillSwiper.min', {
-        slidesPerView: 4,
-        spaceBetween: 5,
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+    reviewSlider.on('slideChange', function () {
+        let currentPagi = document.querySelector('#numberSlides span');
+        currentPagi.innerHTML = reviewSlider.realIndex +1
     });
 }
--- index.php
+++ index.php
@@ -77,4 +77,348 @@
         </div>
     </div>
 </div>
+<section class="servicesSec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="servicesContent">
+                    <div class="secHeading">
+                        <h2>
+                            What can i do for your needs
+                        </h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, dolores libero maxime
+                        natus obcaecati officia omnis sint voluptas. Atque consequuntur debitis, earum exercitationem
+                        facilis impedit ipsum iste placeat veniam voluptate.
+                    </p>
+                    <div class="completedServices">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="serviceCompleted">
+                                        <h3>250+</h3>
+                                        <p>Projects Completed</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="serviceCompleted">
+                                        <h3>100+</h3>
+                                        <p>Community networks</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="serviceCompleted">
+                                        <h3>30+</h3>
+                                        <p>Contacts Remote</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="serviceCompleted">
+                                        <h3>10+</h3>
+                                        <p>Years Experience</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <ul class="services">
+                    <li>
+                        <a href="" class="service">
+                            <figure>
+                                <img src="./assets/images/service1.jpg" alt="">
+                            </figure>
+                            <span class="content">
+                                <h3>UI/UX Design</h3>
+                                <span>
+                                    117 Projects
+                                </span>
+                            </span>
+                            <i class="fas fa-long-arrow-right"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="service">
+                            <figure>
+                                <img src="./assets/images/service2.jpg" alt="">
+                            </figure>
+                            <span class="content">
+                                <h3>Frontend Development</h3>
+                                <span>
+                                    84 Projects
+                                </span>
+                            </span>
+                            <i class="fas fa-long-arrow-right"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="service">
+                            <figure>
+                                <img src="./assets/images/service3.webp" alt="">
+                            </figure>
+                            <span class="content">
+                                <h3>Mobile App Development</h3>
+                                <span>
+                                    32 Projects
+                                </span>
+                            </span>
+                            <i class="fas fa-long-arrow-right"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="workSec">
+    <div class="container">
+        <div class="row servicesRow">
+            <div class="col-md-4">
+                <a href="#" class="serviceCont">
+                    <i class="fad fa-pencil-paintbrush"></i>
+                    <h3>UI/UX Design</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cum distinctio doloremque
+                        dolorum facere facilis itaque iusto molestiae neque nesciunt nihil.
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="serviceCont">
+                    <i class="fal fa-desktop-alt"></i>
+                    <h3>UI/UX Design</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cum distinctio doloremque
+                        dolorum facere facilis itaque iusto molestiae neque nesciunt nihil.
+                    </p>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="serviceCont">
+                    <i class="fal fa-mobile-alt"></i>
+                    <h3>UI/UX Design</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cum distinctio doloremque
+                        dolorum facere facilis itaque iusto molestiae neque nesciunt nihil.
+                    </p>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="workBox">
+                    <div class="content">
+                        <div class="info">
+                            <h4>Mobile App Design</h4>
+                            <h3>Travel Mobile App</h3>
+                        </div>
+                        <a href="#"><i class="fal fa-book"></i>Case Study</a>
+                    </div>
+                    <figure>
+                        <img src="./assets/images/app.png" alt="">
+                    </figure>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="reviewSec">
+    <div class="container">
+        <div class="swiper reviewSwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <figure>
+                                <img src="./assets/images/user1.jpg" alt="">
+                            </figure>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="content">
+                                <i class="fas fa-quote-left"></i>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eos incidunt
+                                    laborum magni
+                                    nam non officiis porro voluptate. Architecto blanditiis consequuntur dolor harum id
+                                    laudantium
+                                    natus necessitatibus, placeat quod totam.
+                                </p>
+                                <div class="info">
+                                    <h3>Edward Collin</h3>
+                                    <span>CTO at XYZ company</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <figure>
+                                <img src="./assets/images/user2.jpg" alt="">
+                            </figure>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="content">
+                                <i class="fas fa-quote-left"></i>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eos incidunt
+                                    laborum magni
+                                    nam non officiis porro voluptate. Architecto blanditiis consequuntur dolor harum id
+                                    laudantium
+                                    natus necessitatibus, placeat quod totam.
+                                </p>
+                                <div class="info">
+                                    <h3>Edward Collin</h3>
+                                    <span>CTO at XYZ company</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <figure>
+                                <img src="./assets/images/user3.jpg" alt="">
+                            </figure>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="content">
+                                <i class="fas fa-quote-left"></i>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eos incidunt
+                                    laborum magni
+                                    nam non officiis porro voluptate. Architecto blanditiis consequuntur dolor harum id
+                                    laudantium
+                                    natus necessitatibus, placeat quod totam.
+                                </p>
+                                <div class="info">
+                                    <h3>Edward Collin</h3>
+                                    <span>CTO at XYZ company</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination swiper-pagination-clickable"></div>
+            <div id="numberSlides">
+                <span>1</span>/3
+            </div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+</section>
+<?php include('include/footer.php') ?>
 <?php include('include/scripts.php') ?>
